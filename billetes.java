aee27049d5b9bfd7628f4abb5a3b170a8c238754
Adrian <aabeijoncarbajo@danielcastelao.org>
2023-09-29 09:38:39 +0200

    hago el primer commit a los archivos originales

---
+++ billetes.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class billetes {
+    public static void main(String[] args) {
+        int b100, b20, b5, m1, total;
+        System.out.print("Billetes de 100€: ");
+        Scanner input = new Scanner(System.in);
+        b100 = input.nextInt()* 100;
+        System.out.print("Billetes de 20€: ");
+        b20 = input.nextInt()* 20;
+        System.out.print("Billetes de 5€: ");
+        b5 = input.nextInt()* 5;
+        System.out.print("Billetes monedas de 1€: ");
+        m1 = input.nextInt();
+        total = b100 + b20 + b5 +m1;
+        System.out.println("\nLa cantidad total es: "+total+" €");
+    }
+}
---
+++ ejemploscanner.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ejemploscanner {
+    public static void main(String[] args) {
+        //area de un rectangulo = base * altura
+/*
+       float base, altura, area;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("El base es : ");
+        base = sc.nextFloat();
+        System.out.print("La altura es : ");
+        altura = sc.nextFloat();
+        area = base*altura;
+        System.out.println("El area del rectangulo es = "+area);
+*/
+        // calculo da lonxitude e area de un circulo lonxitude=2pi*radio(ra) s=n+r^2
+        System.out.print("Teclea el radio del circulo: ");
+        Scanner sc = new Scanner(System.in);
+        float ra = sc.nextFloat();
+        // para PI usamos el Math, como PI es double tenemos que hacer un cast a float
+        float lonxitude = (float) (2*Math.PI*ra);
+        System.out.println("La lonxitude do circulo e: "+lonxitude);
+
+    }
+}
